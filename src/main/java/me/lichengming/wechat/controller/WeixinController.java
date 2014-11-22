0b992a8ad3219f2de53a9e1df78154845cf1ec90
Abner Lee <657143946@qq.com>
2014-11-22 21:14:17 +0800

    controller，echostr 接通开发模式用的

---
+++ src/main/java/me/lichengming/wechat/controller/WeixinController.java
@@ -0,0 +1,23 @@
+package me.lichengming.wechat.controller;
+
+import com.jfinal.aop.ClearInterceptor;
+import com.jfinal.aop.ClearLayer;
+import com.jfinal.core.Controller;
+import me.abnerlee.wechatAPI.Accessor;
+
+/**
+ * Created by Administrator on 14-10-10.
+ */
+@ClearInterceptor(ClearLayer.ALL)
+public class WeixinController extends Controller {
+    public void weixin() {
+        if (getPara("echostr") != null){
+            String ret = "";
+            if (Accessor.checkSignature(this.getRequest())){
+                ret = getPara("echostr");
+            }
+            renderText(ret);
+        }
+        return ;
+    }
+}
