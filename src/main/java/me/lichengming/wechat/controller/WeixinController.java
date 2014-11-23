4eab2b3c1d302b64a2e3c1a36cbd2f5be357873b
Abner Lee <657143946@qq.com>
2014-11-23 21:34:21 +0800

    test autoresponse text message

--- src/main/java/me/lichengming/wechat/controller/WeixinController.java
+++ src/main/java/me/lichengming/wechat/controller/WeixinController.java
@@ -1,18 +1,27 @@
 package me.lichengming.wechat.controller;
 
 import com.jfinal.core.Controller;
-import me.abnerlee.wechatAPI.Accessor;
-import me.abnerlee.wechatAPI.Constant;
+import me.abnerlee.wechatAPI.*;
+
+import java.io.IOException;
 
 /**
  * Created by Administrator on 14-10-10.
  */
 public class WeixinController extends Controller {
-    public void weixin() {
+    public void weixin() throws IOException {
         String ret = "weixin";
         if (getPara("echostr") != null){
             if (Accessor.checkSignature(this.getRequest())){
                 ret = getPara("echostr");
+            }
+        }
+        else {
+            WechatReceiveData msg = WechatReceiveDataParser.getMsgEntity(this.getRequest().getInputStream());
+            if (msg.getMsgType().equals("text")){
+                ret = ConstructWechatXmlString.getTextXml(msg.getFromUserName(), "您发送了："+msg.getContent());
+            } else {
+                ret = ConstructWechatXmlString.getTextXml(msg.getFromUserName(), "更多功能尽请期待");
             }
         }
         renderText(ret);
