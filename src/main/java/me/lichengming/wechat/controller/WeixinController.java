463acd7f755e8e4098e067fe57ad5f6ac88a8237
Abner Lee <657143946@qq.com>
2014-11-23 17:53:07 +0800

    just test wechatAPI

--- src/main/java/me/lichengming/wechat/controller/WeixinController.java
+++ src/main/java/me/lichengming/wechat/controller/WeixinController.java
@@ -2,6 +2,7 @@ package me.lichengming.wechat.controller;
 
 import com.jfinal.core.Controller;
 import me.abnerlee.wechatAPI.Accessor;
+import me.abnerlee.wechatAPI.Constant;
 
 /**
  * Created by Administrator on 14-10-10.
@@ -16,5 +17,14 @@ public class WeixinController extends Controller {
         }
         renderText(ret);
         return ;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Constant.TOKEN);
+        System.out.println(Constant.APPID);
+        System.out.println(Constant.APPSECRET);
+        System.out.println(Constant.ACCESS_TOKEN_URL);
+        System.out.println(Constant.IP_LIST_URL);
+
     }
 }
